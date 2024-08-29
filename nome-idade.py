e692015294b7729fb538ea804319d64bd0f3257f
joca <joaocarloslu11@gmail.com>
2024-08-29 11:50:33 -0300

    Add files via upload

---
+++ nome-idade.py
@@ -0,0 +1,16 @@
+nome = "joca"
+idade = 17
+altura = 1,82
+
+
+
+print("nome: ", nome, "idade: ", idade, "altura: ", altura)
+print(f"nome: {nome}  idade: {idade} altura: {altura}")
+
+nome = input("digite o seu nome: ")
+idade = int("digite a sua idade: ")
+altura = float(input("digite a sua altura: "))
+
+print(f"nome: {nome}  idade: {idade} altura: {altura}")
+
+
